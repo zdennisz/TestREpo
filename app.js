a23308eefefb73489919655119a30b7b41ce509e
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-04-28 14:47:32 +0300

    Added indexHTML

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT||3000
-
+const path = require('path'); 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + './client/public/index.html'));
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

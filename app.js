72e10c2fbdd5454de454e2776e62e4087121971c
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-04-28 14:30:52 +0300

    added rerouting to flower compoment

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT||3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + './client/public/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
